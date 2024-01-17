98d5f859e56c3fa982028d4abce8624743870f62
Rizal Purnawan <rizalpurnawan23@gmail.com>
2024-01-17 13:56:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     packages=find_packages(),
     install_requires=[
         'numpy>=1.23.5',
-        'pandas>=2.1.1',
-        'matplotlib>=3.8.0',
-        'plotly>=5.18.0',
-        'scikit-learn>=1.3.0',
+        'pandas>=2.0.3',
+        'matplotlib>=3.7.4',
+        'plotly>=5.16.1',
+        'scikit-learn>=1.2.2',
         'tensorflow>=2.10.1'
         ],
     url='https://github.com/rizalpurnawan23/AbstrakTS'
