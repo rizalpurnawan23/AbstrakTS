35133da66b042542a865a006c5c6d1312e3da161
Rizal Purnawan <rizalpurnawan23@gmail.com>
2024-01-17 14:11:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='abstrakTS',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.23.5',
